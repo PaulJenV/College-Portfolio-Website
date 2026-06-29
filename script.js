8bdbb466194303ae20f78bb3dabce1be25752a7d
PaulJenV <pjsvillaflor@gmail.com>
2026-06-29 14:19:46 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,79 @@
+const HomeBtn = document.getElementById("HomeBtn");
+const AboutBtn = document.getElementById("AboutBtn");
+const SkillsBtn = document.getElementById("SkillsBtn");
+const ProjectsBtn = document.getElementById("ProjectsBtn");
+const ContactBtn = document.getElementById("ContactBtn");
+
+HomeBtn.addEventListener("click", function () {
+    window.location.href = "Home.html";
+});
+
+AboutBtn.addEventListener("click", function () {
+    window.location.href = "About.html";
+});
+
+SkillsBtn.addEventListener("click", function () {
+    window.location.href = "Skills.html";
+});
+
+ProjectsBtn.addEventListener("click", function () {
+    window.location.href = "Projects.html";
+});
+
+ContactBtn.addEventListener("click", function () {
+    window.location.href = "Contacts.html";
+});
+
+const EducationPopup = document.getElementById("EducationPopup");
+const ClosePopupEdu = document.getElementById("ClosePopupEdu");
+const popupEdu = document.getElementById("popupEdu");
+
+if (EducationPopup) {
+    EducationPopup.addEventListener("click", function () {
+        popupEdu.style.display = "flex";
+    });
+}
+
+if (ClosePopupEdu) {
+    ClosePopupEdu.addEventListener("click", function () {
+        popupEdu.style.display = "none";
+    });
+}
+
+const InterestsPopup = document.getElementById("InterestsPopup");
+const ClosePopupInt = document.getElementById("ClosePopupInt");
+const popupInt = document.getElementById("popupInt");
+
+if (InterestsPopup) {
+    InterestsPopup.addEventListener("click", function () {
+        popupInt.style.display = "flex";
+    });
+}
+
+if (ClosePopupInt) {
+    ClosePopupInt.addEventListener("click", function () {
+        popupInt.style.display = "none";
+    });
+}
+
+const SkillsPopup = document.getElementById("SkillsPopup");
+const ClosePopupSki = document.getElementById("ClosePopupSki");
+const popupSki = document.getElementById("popupSki");
+
+if (SkillsPopup) {
+    SkillsPopup.addEventListener("click", function () {
+        popupSki.style.display = "flex";
+    });
+}
+
+if (ClosePopupSki) {
+    ClosePopupSki.addEventListener("click", function () {
+        popupSki.style.display = "none";
+    });
+}
+
+const DarkMode = document.getElementById("DarkMode");
+
+DarkMode.addEventListener("click", function () {
+    document.body.classList.toggle("darkmode");
+});
